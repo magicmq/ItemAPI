37841071585fa5614351efa9bd98809fca5052e3
magicmq <mquindt@yahoo.com>
2021-02-21 23:04:55 -0600

    Add Javadocs description

--- src/main/java/dev/magicmq/itemapi/utils/exception/MapNotFoundException.java
+++ src/main/java/dev/magicmq/itemapi/utils/exception/MapNotFoundException.java
@@ -1,5 +1,8 @@
 package dev.magicmq.itemapi.utils.exception;
 
+/**
+ * Thrown if a map ID is not found on the server.
+ */
 public class MapNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 111263384692060984L;
